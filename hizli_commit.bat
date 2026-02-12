ef62aa2df033bbc4352ba8a0ba8d5860d70d62c4
Memati8383 <emremsigoksu@gmail.com>
2026-02-12 17:01:14 +0300

    Script Test

--- hizli_commit.bat
+++ hizli_commit.bat
@@ -42,7 +42,7 @@ if errorlevel 1 echo %Y%[-] Bilgi: Yeni degisiklik yok veya commit zaten guncel.
 
 echo.
 echo %B%[3/4] Sunucuyla senkronize ediliyor - Pull...%RESET%
-git pull origin main
+git pull origin main --no-edit
 if errorlevel 1 goto PULL_FAIL
 
 echo.
